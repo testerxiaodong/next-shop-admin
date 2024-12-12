6b6d6c0da822f00910c8dc1048444aefba6d36a2
testerxiaodong <happy_game_dong@163.com>
2024-12-12 18:52:30 +0800

    improve performance

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -4,9 +4,14 @@ import { getCategoryData } from '@/actions/categories'
 import { getLatestUsers } from '@/actions/auth'
 
 const Dashboard = async () => {
-  const monthlyOrders = await getMonthlyOrders()
-  const categoryData = await getCategoryData()
-  const latestUsers = await getLatestUsers()
+  // const monthlyOrders = await getMonthlyOrders()
+  // const categoryData = await getCategoryData()
+  // const latestUsers = await getLatestUsers()
+  const [monthlyOrders, categoryData, latestUsers] = await Promise.all([
+    getMonthlyOrders(),
+    getCategoryData(),
+    getLatestUsers(),
+  ])
 
   return (
     <PageComponent
