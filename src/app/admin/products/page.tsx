9906f6c39485a55bd7d1653bcf11f18ceed0f6ec
testerxiaodong <happy_game_dong@163.com>
2024-12-12 19:10:38 +0800

    improve performance

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -3,8 +3,12 @@ import { getProductsWithCategories } from '@/actions/products'
 import { ProductPageComponent } from '@/app/admin/products/page-component'
 
 export default async function Products() {
-  const categories = await getCategoriesWithProducts()
-  const productsWithCategories = await getProductsWithCategories()
+  // const categories = await getCategoriesWithProducts()
+  // const productsWithCategories = await getProductsWithCategories()
+  const [categories, productsWithCategories] = await Promise.all([
+    getCategoriesWithProducts(),
+    getProductsWithCategories(),
+  ])
 
   return (
     <ProductPageComponent
